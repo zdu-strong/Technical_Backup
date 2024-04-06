0c55906e3f44d92e8bbec014dfcac0c26fdc38d1
zdu <zdu.strong@gmail.com>
2024-04-06 18:25:55 +0900

    #: Add SystemInitScheduled

---
+++ springboot/src/main/java/com/springboot/project/scheduled/SystemInitScheduled.java
@@ -0,0 +1,25 @@
+package com.springboot.project.scheduled;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import com.springboot.project.service.EncryptDecryptService;
+
+@Component
+public class SystemInitScheduled {
+
+    @Autowired
+    private EncryptDecryptService encryptDecryptService;
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 60 * 60 * 1000)
+    public void scheduled() {
+        this.initEncryptDecryptKey();
+    }
+
+    private void initEncryptDecryptKey() {
+        this.encryptDecryptService.getKeyOfAESSecretKey();
+        this.encryptDecryptService.getKeyOfRSAPrivateKey();
+        this.encryptDecryptService.getKeyOfRSAPublicKey();
+    }
+
+}
