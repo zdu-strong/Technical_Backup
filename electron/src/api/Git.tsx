c04407c72430bbf0824f3be15d3fb24a1dbcb473
John <zdu.strong@gmail.com>
2024-04-13 14:55:21 +0900

    #: Optimize code

--- electron/src/api/Git.tsx
+++ electron/src/api/Git.tsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import { TypedJSON } from "typedjson";
 
 export async function getServerGitInfo() {
-  const response = await axios.get<GitPropertiesModel>("/git");
-  response.data = new TypedJSON(GitPropertiesModel).parse(response.data)!;
-  return response;
+  const { data } = await axios.get<GitPropertiesModel>("/git");
+  return new TypedJSON(GitPropertiesModel).parse(data)!;
 }
