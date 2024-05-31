4f5da64461ac0d72520c2cad697f5d9e8d87e050
John Musk <zdu.strong@gmail.com>
2024-05-31 22:51:48 +0900

    #: Formatted code

--- springboot/src/main/java/com/springboot/project/common/config/CorsConfig.java
+++ springboot/src/main/java/com/springboot/project/common/config/CorsConfig.java
@@ -10,7 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(CorsConfiguration.ALL).allowedMethods(CorsConfiguration.ALL)
+        registry.addMapping("/**")
+                .allowedOrigins(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
                 .allowedHeaders(CorsConfiguration.ALL);
     }
 
