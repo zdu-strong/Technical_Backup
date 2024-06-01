63aa6d6bb73008b32ab7d4eba4072efcaef14e99
John Musk <zdu.strong@gmail.com>
2024-06-01 09:00:25 +0900

    #: Update versions of dependencies

--- react/src/reportWebVitals.ts
+++ react/src/reportWebVitals.ts
@@ -1,15 +1,15 @@
-import { ReportHandler } from 'web-vitals';
+import { onCLS, onINP, onLCP, onFCP, onTTFB } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: onCLS(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+export function reportWebVitals(isReport?: boolean) {
+  if (!isReport) {
+    return;
   }
-};
-
-export default reportWebVitals;
+  onCLS(console.log);
+  onINP(console.log);
+  onLCP(console.log);
+  onFCP(console.log);
+  onTTFB(console.log);
+}
