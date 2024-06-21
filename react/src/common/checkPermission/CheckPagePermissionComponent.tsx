cffd08be93c2cb4e6ec916603514c7bab942f1a3
John Musk <zdu.strong@gmail.com>
2024-06-21 21:04:31 +0900

    #: Automatically go to the home page after logging in

--- react/src/common/checkPermission/CheckPagePermissionComponent.tsx
+++ react/src/common/checkPermission/CheckPagePermissionComponent.tsx
@@ -1,21 +1,26 @@
 import api from "@/api";
 import LoadingOrErrorComponent from "@/common/LoadingOrErrorComponent/LoadingOrErrorComponent";
-import { GlobalUserInfo } from "@/common/Server";
-import { observer, useMobxState, useMount } from "mobx-react-use-autorun";
-import { ReactNode, useEffect } from "react";
+import { GlobalUserInfo, handleErrorWhenNotSignInToSignIn } from "@/common/Server";
+import { observer, useMobxEffect, useMobxState, useMount } from "mobx-react-use-autorun";
+import { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 import { ReplaySubject, concatMap, from } from "rxjs";
 import { v1 } from "uuid";
 
 export default observer((props: {
   children: ReactNode,
-  isAutoLogin?: boolean
-  checkIsSignIn?: boolean
+  isAutoLogin?: boolean,
+  checkIsSignIn?: boolean,
+  checkIsNotSignIn?: boolean,
 }) => {
 
   const state = useMobxState({
     subject: new ReplaySubject<void>(),
     ready: false,
     error: null as any,
+  }, {
+    ...props,
+    navigate: useNavigate()
   })
 
   useMount(async (subscription) => {
@@ -24,10 +29,8 @@ export default observer((props: {
         state.ready = false;
         state.error = null;
         try {
-          if (props.isAutoLogin) {
-            if (!GlobalUserInfo.accessToken) {
-              await api.Authorization.signUp(v1(), "visitor", []);
-            }
+          if (state.isAutoLogin && !GlobalUserInfo.accessToken) {
+            await api.Authorization.signUp(v1(), "visitor", []);
           }
           check();
           state.ready = true;
@@ -36,26 +39,34 @@ export default observer((props: {
         }
       })()))
     ).subscribe());
+
   })
 
-  useEffect(() => {
+  useMobxEffect(() => {
     state.subject.next();
-  }, [props.isAutoLogin, props.checkIsSignIn, state.subject])
+  }, [state.isAutoLogin, state.checkIsSignIn, state.subject, state.checkIsNotSignIn])
+
+  useMobxEffect(() => {
+    if (state.checkIsNotSignIn && GlobalUserInfo.accessToken) {
+      state.navigate("/");
+    }
+  }, [state.checkIsNotSignIn, GlobalUserInfo.accessToken])
 
   function check() {
     checkIsSignIn();
   }
 
   function checkIsSignIn() {
-    if (!props.checkIsSignIn) {
+    if (!state.checkIsSignIn) {
       return;
     }
     if (!GlobalUserInfo.accessToken) {
+      handleErrorWhenNotSignInToSignIn({ status: 401 });
       throw new Error("Please login first and then visit")
     }
   }
 
   return <LoadingOrErrorComponent ready={state.ready} error={state.error} >
-    {props.children}
+    {state.children}
   </LoadingOrErrorComponent>
 })
--- react/src/router/index.tsx
+++ react/src/router/index.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFound from '@/component/NotFound/NotFound';
-import SignIn from '@/component/SignIn/SignIn';
 import SignUp from '@/component/SignUp/SignUp';
 import Home from '@/component/Home/Home';
-import MessageEntryPage from '@/router/page/MessageEntryPage';
+import MessagePage from '@/router/page/MessagePage';
 import GitInfo from '@/component/GitInfo';
+import SignInPage from '@/router/page/SignInPage'
 
 export default (
   <BrowserRouter>
     <Routes>
-      <Route index element={MessageEntryPage} />
-      <Route path="/chat" element={MessageEntryPage} />
-      <Route path="/sign_in" element={<SignIn />} />
+      <Route index element={MessagePage} />
+      <Route path="/chat" element={MessagePage} />
+      <Route path="/sign_in" element={SignInPage} />
       <Route path="/sign_up" element={<SignUp />} />
       <Route path="/git" element={<GitInfo />} />
       <Route path="/home" element={<Home />} />
