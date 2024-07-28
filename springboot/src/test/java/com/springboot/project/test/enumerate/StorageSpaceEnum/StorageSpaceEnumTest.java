c7c15708b78d73d608f91b54739b98b04bb3fa06
John Musk <zdu.strong@gmail.com>
2024-07-28 18:37:53 +0900

    #: Add Unit tests

---
+++ springboot/src/test/java/com/springboot/project/test/enumerate/StorageSpaceEnum/StorageSpaceEnumTest.java
@@ -0,0 +1,15 @@
+package com.springboot.project.test.enumerate.StorageSpaceEnum;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import com.springboot.project.enumerate.StorageSpaceEnum;
+import com.springboot.project.test.common.BaseTest.BaseTest;
+
+public class StorageSpaceEnumTest extends BaseTest {
+
+    @Test
+    public void test() {
+        assertEquals(86400000, StorageSpaceEnum.TEMP_FILE_SURVIVAL_DURATION.toMillis());
+    }
+
+}
