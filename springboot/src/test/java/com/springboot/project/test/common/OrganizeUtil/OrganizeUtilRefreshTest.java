0436642736f1bcdf86fcb615e2b2ec09c3575afb
John Musk <zdu.strong@gmail.com>
2024-08-03 21:40:03 +0900

    #: Add unit tests

---
+++ springboot/src/test/java/com/springboot/project/test/common/OrganizeUtil/OrganizeUtilRefreshTest.java
@@ -0,0 +1,29 @@
+package com.springboot.project.test.common.OrganizeUtil;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.springboot.project.model.OrganizeModel;
+import com.springboot.project.test.common.BaseTest.BaseTest;
+
+public class OrganizeUtilRefreshTest extends BaseTest {
+
+    private String organizeId;
+
+    @Test
+    public void test() {
+        this.organizeUtil.refresh(organizeId);
+        var result = this.organizeService.searchByName(1L, 20L, "Son Gohan", this.organizeId);
+        assertEquals(1, result.getTotalRecord());
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        var organizeModel = new OrganizeModel().setName("Super Saiyan Son Goku");
+        this.organizeId = this.organizeService.create(organizeModel).getId();
+        this.organizeService.create(
+                new OrganizeModel().setName("Son Gohan").setParent(new OrganizeModel().setId(organizeId)));
+        var result = this.organizeService.searchByName(1L, 20L, "Son Gohan", this.organizeId);
+        assertEquals(0, result.getTotalRecord());
+    }
+}
