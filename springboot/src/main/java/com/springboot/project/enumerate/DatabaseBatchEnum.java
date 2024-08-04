8274e9d17568db84098a91e44ebd60457913be24
John Musk <zdu.strong@gmail.com>
2024-08-04 12:51:27 +0900

    #: Change the batch size to 35

--- springboot/src/main/java/com/springboot/project/enumerate/DatabaseBatchEnum.java
+++ springboot/src/main/java/com/springboot/project/enumerate/DatabaseBatchEnum.java
@@ -5,5 +5,5 @@ public class DatabaseBatchEnum {
     /**
      * Number of data items to be processed in batches
      */
-    public final static long batchSize = 50;
+    public final static long batchSize = 35;
 }
