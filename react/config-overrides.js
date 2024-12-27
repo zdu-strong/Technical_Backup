6137033b7062359726e1dadd6fba427fed5d1d06
John <zdu.strong@gmail.com>
2024-12-27 22:26:48 +0900

    #: Update version of node-polyfill-webpack-plugin

--- react/config-overrides.js
+++ react/config-overrides.js
@@ -4,7 +4,19 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
 module.exports = function override(config) {
   config.plugins = (config.plugins || []).concat([
-    new NodePolyfillPlugin()
+    new NodePolyfillPlugin({
+      additionalAliases: [
+        "_stream_duplex",
+        "_stream_passthrough",
+        "_stream_readable",
+        "_stream_transform",
+        "_stream_writable",
+        "console",
+        "domain",
+        "process",
+        "punycode",
+      ]
+    })
   ]);
   config.resolve.alias['@'] = path.join(__dirname, 'src');
   config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
