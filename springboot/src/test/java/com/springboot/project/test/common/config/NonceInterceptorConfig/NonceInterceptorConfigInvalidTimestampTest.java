1b7f0b4af8b8312343b31c63b3fa9d4f814a2ce3
John <zdu.strong@gmail.com>
2025-01-10 23:23:18 +0900

    #: Modify unit tests

--- springboot/src/test/java/com/springboot/project/test/common/config/NonceInterceptorConfig/NonceInterceptorConfigInvalidTimestampTest.java
+++ springboot/src/test/java/com/springboot/project/test/common/config/NonceInterceptorConfig/NonceInterceptorConfigInvalidTimestampTest.java
@@ -3,8 +3,6 @@ package com.springboot.project.test.common.config.NonceInterceptorConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Date;
-import org.apache.commons.lang3.time.FastDateFormat;
 import org.apache.hc.core5.net.URIBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +10,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import com.fasterxml.uuid.Generators;
 import com.springboot.project.test.common.BaseTest.BaseTest;
 
@@ -36,16 +33,6 @@ public class NonceInterceptorConfigInvalidTimestampTest extends BaseTest {
     @BeforeEach
     public void beforeEach() throws URISyntaxException {
         this.nonce = Generators.timeBasedReorderedGenerator().generate().toString();
-        this.timestamp = FastDateFormat.getInstance(this.dateFormatProperties.getUTC()).format(new Date());
-        URI url = new URIBuilder("/").build();
-        var httpHeaders = new HttpHeaders();
-        httpHeaders.set("X-Nonce", nonce);
-        httpHeaders.set("X-Timestamp", timestamp);
-        ResponseEntity<String> response = this.testRestTemplate.exchange(url, HttpMethod.GET,
-                new HttpEntity<>(httpHeaders), String.class);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(13, response.getBody().length());
-        assertEquals("Hello, World!", response.getBody());
         this.timestamp = "Invalid Timestamp";
     }
 }
