b87c9117ec6a45a50b451775e44aa6ed9c7c8580
John <zdu.strong@gmail.com>
2025-01-13 22:46:47 +0900

    #: Modify PhoneEnum

--- rust/src/action/buy_phone/mod.rs
+++ rust/src/action/buy_phone/mod.rs
@@ -1,3 +1,2 @@
-pub mod buy_phone;
 pub mod tom_buy_phone;
 pub mod jerry_buy_phone;
--- rust/src/enumable/phone_enum.rs
+++ rust/src/enumable/phone_enum.rs
@@ -1,8 +1,33 @@
 use crate::model::iphone_model::IPhoneModel;
 use crate::model::pixel_model::PixelModel;
+use async_recursion::async_recursion;
 
 #[derive(Debug, Clone)]
 pub enum PhoneEnum {
     Pixel(PixelModel),
     IPhone(IPhoneModel),
+}
+
+impl PhoneEnum {
+    #[async_recursion]
+    pub async fn buy(&mut self) {
+        match self {
+            PhoneEnum::IPhone(iphone) => iphone.buy().await,
+            PhoneEnum::Pixel(pixel) => pixel.buy().await,
+        };
+    }
+}
+
+impl IPhoneModel {
+    #[async_recursion]
+    pub async fn buy(&mut self) {
+        println!("{} cost {} buy iphone", self.owner, self.price);
+    }
+}
+
+impl PixelModel {
+    #[async_recursion]
+    pub async fn buy(&mut self) {
+        println!("{} cost {} buy pixel", self.owner, self.price);
+    }
 }
