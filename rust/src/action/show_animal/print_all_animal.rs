6d557129049b665b8393e8927c71b14b7f0a7f81
John <zdu.strong@gmail.com>
2025-01-16 20:43:22 +0900

    #: Modify AnimalEnum

--- rust/src/action/show_animal/print_all_animal.rs
+++ rust/src/action/show_animal/print_all_animal.rs
@@ -3,19 +3,19 @@ use async_recursion::async_recursion;
 
 #[async_recursion]
 pub async fn print_all_animal() {
-    let ref mut animal_list = AnimalEnum::values().await;
+    let ref mut animal_list = AnimalEnum::values();
     println!(
         "All animal is {}",
         serde_json::to_string(animal_list).unwrap()
     );
     println!(
         "{} is {} year old",
-        AnimalEnum::Tiger.name().await,
-        AnimalEnum::Tiger.age().await
+        AnimalEnum::Tiger.name(),
+        AnimalEnum::Tiger.age()
     );
     println!(
         "{} is {} years old",
-        AnimalEnum::Dog.name().await,
-        AnimalEnum::Dog.age().await
+        AnimalEnum::Dog.name(),
+        AnimalEnum::Dog.age()
     );
 }
--- rust/src/enumable/animal_enum.rs
+++ rust/src/enumable/animal_enum.rs
@@ -1,4 +1,3 @@
-use async_recursion::async_recursion;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -9,22 +8,19 @@ pub enum AnimalEnum {
 }
 
 impl AnimalEnum {
-    #[async_recursion]
-    pub async fn values() -> Vec<AnimalEnum> {
+    pub fn values() -> Vec<AnimalEnum> {
         return [AnimalEnum::Tiger, AnimalEnum::Dog].to_vec();
     }
 }
 
 impl AnimalEnum {
-    #[async_recursion]
-    pub async fn name(&mut self) -> String {
+    pub fn name(&mut self) -> String {
         return match self {
             AnimalEnum::Tiger => "Tiger".to_string(),
             AnimalEnum::Dog => "Dog".to_string(),
         };
     }
-    #[async_recursion]
-    pub async fn age(&mut self) -> u32 {
+    pub fn age(&mut self) -> u32 {
         return match self {
             AnimalEnum::Tiger => 2,
             AnimalEnum::Dog => 1,
