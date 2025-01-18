9538360dfae968159cafd15b1c94e6f57b9f3594
John <zdu.strong@gmail.com>
2025-01-18 23:14:06 +0900

    #: Modify PaginationModel

--- electron/src/model/PaginationMode.tsx
+++ electron/src/model/PaginationMode.tsx
@@ -18,16 +18,39 @@ export class PaginationModel<T> {
     pageNum: number,
     pageSize: number,
     stream: linq.IEnumerable<T>
+  );
+
+  constructor(otherPaginationModel: any);
+
+  constructor(
+    pageNum?: number,
+    pageSize?: number,
+    stream?: linq.IEnumerable<T>
   ) {
     makeAutoObservable(this);
-    if (pageNum < 1) {
+    if (typeof pageNum === "object" && typeof pageSize === "undefined" && typeof stream === "undefined") {
+      const otherPaginationModel = pageNum as any;
+      this.pageNum = otherPaginationModel.pageNum;
+      this.pageSize = otherPaginationModel.pageSize;
+      this.totalRecord = otherPaginationModel.totalRecord;
+      this.totalPage = otherPaginationModel.totalPage;
+      this.list = otherPaginationModel.list;
+      if (!(typeof this.pageNum === "number" && typeof this.pageSize === "number" && typeof this.totalRecord === "number" && typeof this.totalPage === "number" && typeof this.list === "object" && this.list instanceof Array)) {
+        throw new Error("Incorrect paging behavior");
+      }
+      return;
+    }
+    if (!(typeof pageNum === "number" && typeof pageSize === "number" && typeof stream === "object")) {
+      return;
+    }
+    if (pageNum! < 1) {
       throw new Error("The page number cannot be less than 1");
     }
-    if (pageSize < 1) {
+    if (pageSize! < 1) {
       throw new Error("The page size cannot be less than 1");
     }
 
-    if (pageNum !== Math.floor(pageNum)) {
+    if (pageNum !== Math.floor(pageNum!)) {
       throw new Error("The page number must be an integer");
     }
 
@@ -35,9 +58,9 @@ export class PaginationModel<T> {
       throw new Error("The page size must be an integer");
     }
 
-    const totalRecord = stream.count();
+    const totalRecord = (stream as linq.IEnumerable<T>).count();
     const totalPage = Math.floor(mathjs.divide(totalRecord, pageSize)) + mathjs.mod(totalRecord, pageSize) > 0 ? 1 : 0;
-    const list = stream
+    const list = (stream as linq.IEnumerable<T>)
       .skip(mathjs.multiply(pageNum - 1, pageSize))
       .take(pageSize)
       .toArray();
