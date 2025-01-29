e128ab5db4029146378b22948b5e97236732d4b9
John <zdu.strong@gmail.com>
2025-01-29 22:40:23 +0900

    #: Modify AnimalEnum

--- rust/src/enumable/animal_enum.rs
+++ rust/src/enumable/animal_enum.rs
@@ -6,13 +6,21 @@ use strum_macros::EnumIter;
 #[derive(Debug, Clone, Serialize, Deserialize, EnumIter)]
 #[serde(rename_all = "camelCase")]
 pub enum AnimalEnum {
-    Tiger,
-    Dog,
+    Tiger = 1,
+    Dog = 2,
 }
 
 impl AnimalEnum {
     pub fn values() -> Vec<AnimalEnum> {
         AnimalEnum::iter().collect::<Vec<_>>()
+    }
+
+    pub fn of(id: usize) -> AnimalEnum {
+        AnimalEnum::values()
+            .into_iter()
+            .filter(|s| (s.clone() as usize) == id)
+            .next()
+            .unwrap()
     }
 
     pub fn name(&mut self) -> String {
