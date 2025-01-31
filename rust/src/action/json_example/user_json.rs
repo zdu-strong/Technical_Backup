240e678b394c1771c667648f55c2436dfe0f2675
John <zdu.strong@gmail.com>
2025-02-01 07:05:55 +0900

    #: Modify pagination_example

--- rust/src/action/json_example/user_json.rs
+++ rust/src/action/json_example/user_json.rs
@@ -3,26 +3,24 @@ use chrono::Local;
 use uuid::Uuid;
 
 pub async fn json_to_string() -> String {
-    let ref mut user_list = [UserModel {
+    let ref mut user_list = vec![UserModel {
         id: Uuid::new_v4().to_string(),
         name: "Tom".to_string(),
         create_date: Some(Local::now()),
         update_date: Some(Local::now()),
-    }]
-    .to_vec();
+    }];
     let ref mut json_string = serde_json::to_string(user_list).unwrap();
     println!("{}", json_string);
     return json_string.clone();
 }
 
 pub async fn string_to_json() {
-    let ref mut user_list = [UserModel {
+    let ref mut user_list = vec![UserModel {
         id: Uuid::new_v4().to_string(),
         name: "Jerry".to_string(),
         create_date: Some(Local::now()),
         update_date: Some(Local::now()),
-    }]
-    .to_vec();
+    }];
     let ref mut json_string = serde_json::to_string(user_list).unwrap();
     let ref mut user_list: Vec<UserModel> = serde_json::from_str(json_string).unwrap();
     println!("{}", serde_json::to_string(user_list).unwrap());
--- rust/src/action/pagination/pagination.rs
+++ rust/src/action/pagination/pagination.rs
@@ -1,7 +1,7 @@
 use crate::model::pagination_model::PaginationModel;
 
 pub async fn pagination_example() {
-    let ref mut pagination_model = PaginationModel::new(1, 1, ["tom", "jerry"].to_vec());
+    let ref mut pagination_model = PaginationModel::new(1, 1, vec!["tom", "jerry"]);
     let ref mut pagination_json_string = serde_json::to_string(pagination_model).unwrap();
     println!("pagination_model: {}", pagination_json_string);
     let ref mut pagination_one_model: PaginationModel<String> =
