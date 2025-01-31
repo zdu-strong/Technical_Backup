f03328021eb490f96c21537c3a29db0899480a09
John <zdu.strong@gmail.com>
2025-01-31 23:03:48 +0900

    #: Modify SystemRoleEnum

--- springboot/src/main/java/com/springboot/project/enums/SystemRoleEnum.java
+++ springboot/src/main/java/com/springboot/project/enums/SystemRoleEnum.java
@@ -1,29 +1,24 @@
 package com.springboot.project.enums;
 
-import java.util.Arrays;
 import java.util.List;
 import org.nd4j.common.primitives.Optional;
 import cn.hutool.core.util.EnumUtil;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum SystemRoleEnum {
 
-    SUPER_ADMIN("SUPER_ADMIN", false, SystemPermissionEnum.SUPER_ADMIN),
-    ORGANIZE_MANAGE("ORGANIZE_MANAGE", true, SystemPermissionEnum.ORGANIZE_MANAGE),
-    ORGANIZE_VIEW("ORGANIZE_VIEW", true, SystemPermissionEnum.ORGANIZE_VIEW);
+    SUPER_ADMIN("SUPER_ADMIN", false, List.of(SystemPermissionEnum.SUPER_ADMIN)),
+    ORGANIZE_MANAGE("ORGANIZE_MANAGE", true, List.of(SystemPermissionEnum.ORGANIZE_MANAGE)),
+    ORGANIZE_VIEW("ORGANIZE_VIEW", true, List.of(SystemPermissionEnum.ORGANIZE_VIEW));
 
     private String value;
 
     private Boolean isOrganizeRole;
 
     private List<SystemPermissionEnum> permissionList;
-
-    private SystemRoleEnum(String value, boolean isOrganizeRole, SystemPermissionEnum... permissionArray) {
-        this.value = value;
-        this.isOrganizeRole = isOrganizeRole;
-        this.permissionList = Arrays.asList(permissionArray);
-    }
 
     public static SystemRoleEnum parseValue(String value) {
         return Optional.ofNullable(EnumUtil.getBy(SystemRoleEnum::getValue, value)).get();
