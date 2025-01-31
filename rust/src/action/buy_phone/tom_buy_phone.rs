905842ecf5e60fb10e3ef6a429cce9ad9932d7cb
John <zdu.strong@gmail.com>
2025-02-01 07:09:26 +0900

    #: Modify tom_buy_phone function

--- rust/src/action/buy_phone/tom_buy_phone.rs
+++ rust/src/action/buy_phone/tom_buy_phone.rs
@@ -3,17 +3,17 @@ use crate::service::phone_service::phone_service::PhoneService;
 use crate::service::phone_service::pixel_service::PixelService;
 
 pub async fn tom_buy_phone() {
-    let phone_list: Vec<Box<dyn PhoneService>> = vec![
+    let ref mut phone_list: Vec<Box<dyn PhoneService>> = vec![
         Box::new(IPhoneService {
             price: "10,000".to_string(),
             owner: "Tom".to_string(),
-        }),
+        }) as Box<dyn PhoneService>,
         Box::new(PixelService {
             price: "10,000".to_string(),
             owner: "Tom".to_string(),
         }),
     ];
-    for mut phone in phone_list {
+    for phone in phone_list {
         phone.buy().await;
     }
 }
