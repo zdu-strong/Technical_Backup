e115c1c81483dd09a77fd48266e4ffcff726cf7c
john <zdu.strong@gmail.com>
2025-03-20 18:57:58 +0900

    Modify height of page

--- react/src/component/SystemMenu/MainMenu.tsx
+++ react/src/component/SystemMenu/MainMenu.tsx
@@ -1,23 +1,43 @@
-import { observer } from 'mobx-react-use-autorun';
+import { observer, useMobxState } from 'mobx-react-use-autorun';
 import { ReactNode } from 'react';
-import { type Navigation } from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { ReactRouterAppProvider } from '@toolpad/core/react-router'
 import UserInfoMenu from '@/component/SystemMenu/UserInfoMenu';
 import getNavigation from '@/component/SystemMenu/js/getNavigation';
+import { stylesheet } from 'typestyle';
 
-const NAVIGATION: Navigation = getNavigation();
+
+const css = stylesheet({
+  dashboardLayoutContainer: {
+    $nest: {
+      "& > div.MuiBox-root": {
+        display: "flex",
+        flex: "1 1 auto",
+        flexDirection: "column",
+        height: "unset",
+      }
+    }
+  }
+});
 
 export default observer((props: {
   children: ReactNode
 }) => {
+
+  const state = useMobxState(() => ({
+    navigation: getNavigation(),
+  }))
+
   return <ReactRouterAppProvider
-    navigation={NAVIGATION}
+    navigation={state.navigation}
+    branding={{ title: "", logo: "" }}
   >
-    <DashboardLayout
-      slots={{ toolbarActions: UserInfoMenu }}
-    >
-      {props.children}
-    </DashboardLayout>
+    <div className={`flex flex-col flex-auto ${css.dashboardLayoutContainer}`}>
+      <DashboardLayout
+        slots={{ toolbarActions: UserInfoMenu }}
+      >
+        {props.children}
+      </DashboardLayout>
+    </div>
   </ReactRouterAppProvider>
 })
