d90e9a3d7dfb3b9a38f695b84af77370b695c045
john <zdu.strong@gmail.com>
2025-03-20 16:12:11 +0900

    Fix unit tests

--- capacitor/src/common/router/BrowserRouterOrHashRouter.tsx
+++ capacitor/src/common/router/BrowserRouterOrHashRouter.tsx
@@ -5,8 +5,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 export default observer((props: { children: React.ReactNode }) => {
 
   if (process.env.NODE_ENV === "production") {
-    return (<HashRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>{props.children}</HashRouter>);
+    return (<HashRouter>{props.children}</HashRouter>);
   } else {
-    return (<BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>{props.children}</BrowserRouter>);
+    return (<BrowserRouter>{props.children}</BrowserRouter>);
   }
 })
