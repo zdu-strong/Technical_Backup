943903a36e3d0f936152900cc09f44b56a8213b0
John Williams <zdu.strong@gmail.com>
2025-04-11 23:33:59 +0900

    Modify SuperAdminRoleManage

--- react/src/component/SuperAdminRoleManage/SuperAdminRoleManage.tsx
+++ react/src/component/SuperAdminRoleManage/SuperAdminRoleManage.tsx
@@ -11,31 +11,7 @@ import { faSearch, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FormattedMessage } from "react-intl";
 import { PaginationModel } from "@/model/PaginationModel";
 import { MessageService } from "@/common/MessageService";
-
-const columns: GridColDef<SystemRoleModel>[] = [
-  {
-    headerName: 'ID',
-    field: 'id',
-    width: 290
-  },
-  {
-    renderHeader: () => <FormattedMessage id="Name" defaultMessage="Name" />,
-    field: 'name',
-    width: 150,
-    flex: 1,
-  },
-  {
-    renderHeader: () => <FormattedMessage id="CreateDate" defaultMessage="Create Date" />,
-    field: 'createDate',
-    description: 'This column has a value getter and is not sortable.',
-    renderCell: (row) => {
-      return <div>
-        {format(row.row.createDate, "yyyy-MM-dd HH:mm:ss")}
-      </div>
-    },
-    width: 150,
-  },
-];
+import SuperAdminRoleDetailButton from "./SuperAdminRoleDetailButton";
 
 export default observer(() => {
 
@@ -44,6 +20,39 @@ export default observer(() => {
     loading: true,
     error: null as any,
     systemRolePaginationModel: null as any as PaginationModel<SystemRoleModel>,
+    columns: [
+      {
+        headerName: 'ID',
+        field: 'id',
+        width: 290
+      },
+      {
+        renderHeader: () => <FormattedMessage id="Name" defaultMessage="Name" />,
+        field: 'name',
+        width: 150,
+        flex: 1,
+      },
+      {
+        renderHeader: () => <FormattedMessage id="CreateDate" defaultMessage="Create Date" />,
+        field: 'createDate',
+        description: 'This column has a value getter and is not sortable.',
+        renderCell: (row) => {
+          return <div>
+            {format(row.row.createDate, "yyyy-MM-dd HH:mm:ss")}
+          </div>
+        },
+        width: 150,
+      },
+      {
+        renderHeader: () => <FormattedMessage id="Operation" defaultMessage="Operation" />,
+        field: '',
+        renderCell: (row) => <SuperAdminRoleDetailButton
+          id={row.row.id}
+          searchByPagination={searchByPagination}
+        />,
+        width: 150,
+      },
+    ] as GridColDef<SystemRoleModel>[],
   }, {
     dataGridRef: useGridApiRef(),
   });
@@ -89,7 +98,7 @@ export default observer(() => {
               rowCount={state.systemRolePaginationModel.totalRecords}
               paginationMode="server"
               getRowId={(s) => s.id}
-              columns={columns}
+              columns={state.columns}
               autoPageSize
               disableRowSelectionOnClick
               disableColumnMenu
