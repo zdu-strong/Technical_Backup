43402a5cd1430d2fe0a472b719633d8196a142f8
John Williams <zdu.strong@gmail.com>
2025-04-12 15:56:27 +0900

    Modify PaginationModel

--- rust/src/model/pagination_model.rs
+++ rust/src/model/pagination_model.rs
@@ -6,13 +6,13 @@ use serde::Serialize;
 pub struct PaginationModel<T> {
     pub page_num: usize,
     pub page_size: usize,
-    pub total_record: usize,
-    pub total_page: usize,
-    pub list: Vec<T>,
+    pub total_records: usize,
+    pub total_pages: usize,
+    pub items: Vec<T>,
 }
 
 impl<T> PaginationModel<T> {
-    pub fn new(page_num: usize, page_size: usize, list: Vec<T>) -> PaginationModel<T> {
+    pub fn new(page_num: usize, page_size: usize, items: Vec<T>) -> PaginationModel<T> {
         if page_num < 1 {
             panic!("page_num cannot be less than 1")
         }
@@ -22,9 +22,9 @@ impl<T> PaginationModel<T> {
         PaginationModel {
             page_num: page_num,
             page_size: page_size,
-            total_record: list.len(),
-            total_page: (list.len() / page_size) + (if list.len() % page_size > 0 { 1 } else { 0 }),
-            list: list
+            total_records: items.len(),
+            total_pages: (items.len() / page_size) + (if items.len() % page_size > 0 { 1 } else { 0 }),
+            items: items
                 .into_iter()
                 .skip((page_num - 1) * page_size)
                 .take(page_size)
