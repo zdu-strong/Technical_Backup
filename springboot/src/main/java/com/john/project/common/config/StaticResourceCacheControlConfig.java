c5c413f36c1aac14f247eaf7260533102e5435ce
John Williams <zdu.strong@gmail.com>
2025-04-19 14:57:41 +0900

    Add CacheControlConfig

---
+++ springboot/src/main/java/com/john/project/common/config/StaticResourceCacheControlConfig.java
@@ -0,0 +1,19 @@
+package com.john.project.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceCacheControlConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noStore());
+    }
+
+}
